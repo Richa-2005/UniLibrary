70762290dc8e06fb0880d4af90490cc250b2572b
Richa Gupta <richamgupta2005@gmail.com>
2025-10-28 16:56:01 +0530

    Application Set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Route,Router } from 'react-router-dom'
+export default function App() {
+  return (
+    <>
+     <Router>
+      
+     </Router>
+    </>
+  )
+}
+
