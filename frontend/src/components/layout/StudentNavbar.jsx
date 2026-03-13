8a32e62c57131e09d80002ee6d5e425efcb093f0
Richa Gupta <richamgupta2005@gmail.com>
2026-03-13 22:03:59 +0530

    Final improvements before deploy

--- frontend/src/components/layout/StudentNavbar.jsx
+++ frontend/src/components/layout/StudentNavbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import styles from './Navbar.module.css'; 
+import styles from './NavBar.module.css'; 
 import StudentProfileSidebar from './StudentProfileSidebar';
 
 const StudentNavbar = () => {
